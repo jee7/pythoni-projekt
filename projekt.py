3478639d2e6ae865fdd3ba29be7721ae812b50dc
jee7 <raimondtunnel@hotmail.com>
2021-11-28 17:54:19 +0200

    Tegin töö ära.

---
+++ projekt.py
@@ -0,0 +1,25 @@
+import pygame
+
+pygame.init()
+aken = pygame.display.set_mode([640, 480])
+
+mäng_töötab = True
+
+lind = pygame.image.load("tom.png")
+linnu_x = 10
+linnu_y = 200
+
+while mäng_töötab:
+    hiire_x, hiire_y = pygame.mouse.get_pos()
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            mäng_töötab = False
+
+    linnu_x = hiire_x
+    linnu_y = hiire_y
+
+    aken.fill([0, 0, 0])
+    aken.blit(lind, [linnu_x, linnu_y])
+    pygame.display.flip()
+
+pygame.quit()
